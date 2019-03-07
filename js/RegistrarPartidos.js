d01337d55988293f46d31e5909e609fcee52934c
AlanOsvaldo1 <alan.mayorga@alumnouttehuacan.edu.mx>
2019-03-06 19:50:57 -0600

    Alan

--- js/RegistrarPartidos.js
+++ js/RegistrarPartidos.js
@@ -2,6 +2,26 @@
 var db = firebase.firestore();
 var storage = firebase.storage();
 
+var idLiga;
+
+(function user() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+
+            db.collection("admin").where("userID", "==", user.uid).get().then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    user = doc.id;
+                    idLiga = doc.data().idliga;
+                    leerPartidos();
+                });
+            });
+        } else {
+            console.log("entro");
+
+        }
+    });
+})();
+
 (function leerEquipos() {
     var selection_roll = document.getElementById('local');
     var selection_roll2 = document.getElementById('visitante');
@@ -12,15 +32,19 @@ var storage = firebase.storage();
         selection_roll2.innerHTML = '';
         querySnapshot.forEach((doc) => {
             selection_roll.innerHTML += ` 
-            <option  value="" disabled selected>${doc.data().nombreEquipo}</option>`;
+            <option>${doc.data().nombreEquipo}</option>`;
             selection_roll2.innerHTML += ` 
-            <option value="" disabled selected>${doc.data().nombreEquipo}</option>`;
+            <option>${doc.data().nombreEquipo}</option>`;
         });
-      
+
     });
-    console.log(selection_roll); 
 })();
+
 function crearPartido() {
+    var contenedor = document.getElementById('contCarga');
+    contenedor.style.visibility = 'visible';
+    contenedor.style.opacity = '100';
+
     var local = document.getElementById('local').value;
     var visitante = document.getElementById('visitante').value;
     var fecha = document.getElementById('fecha').value;
@@ -33,6 +57,7 @@ function crearPartido() {
         visitante: visitante,
         fecha: fecha,
         hora: hora,
+        idLiga: idLiga,
         lugar: lugar,
         jornada: jornada,
     }).then(function (docRef) {
@@ -46,38 +71,50 @@ function crearPartido() {
             document.getElementById('hora').value = '';
             document.getElementById('lugar').value = '';
             document.getElementById('jornada').value = '';
-            window.location = "Partidos.html";
-        })
+            var contenedor = document.getElementById('contCarga');
+            contenedor.style.visibility = 'hidden';
+            contenedor.style.opacity = '0';
+        }).catch((error) => {
+            console.log(error);
+            var contenedor = document.getElementById('contCarga');
+            contenedor.style.visibility = 'hidden';
+            contenedor.style.opacity = '0';
+        });
     }).catch(function (error) {
         console.error("Error adding document: ", error);
-    });
-}
-(function leerLigas() {
-    var tabla_arbitros = document.getElementById('tabla');
-    tabla_arbitros.innerHTML = '';
-    db.collection("Partido").onSnapshot((querySnapshot) => {
-        tabla_arbitros.innerHTML = '';
-        querySnapshot.forEach((doc) => {
-
-            tabla_arbitros.innerHTML += `
-        <tr>
-          <td>${doc.data().local}</td>
-          <td>${doc.data().visitante}</td>
-          <td>${doc.data().fecha}</td>
-          <td>${doc.data().hora}</td>
-          <td>${doc.data().lugar}</td>
-          <td>${doc.data().jornada}</td>
-              <td><h4><i class="fas fa-sync-alt"  data-toggle="modal" data-target=".bd-example-modal-lg" onclick="actualizarPartido('${doc.id}','${doc.data().local}',
-          '${doc.data().visitante}','${doc.data().fecha}','${doc.data().hora}','${doc.data().lugar}','${doc.data().jornada}')"></i></h4></td>
-          <td><h4><i class="fas fa-trash-alt" onclick="eliminarPartido('${doc.id}')"></i></h4></td>
-        </tr>
-        `;
-        });
         var contenedor = document.getElementById('contCarga');
         contenedor.style.visibility = 'hidden';
         contenedor.style.opacity = '0';
     });
-})();
+}
+
+function leerPartidos() {
+    var tabla_arbitros = document.getElementById('tabla');
+    tabla_arbitros.innerHTML = '';
+    db.collection("Partido").where("idLiga", "==", idLiga).onSnapshot(function (querySnapshot) {
+        tabla_arbitros.innerHTML = '';
+        querySnapshot.forEach(function (doc) {
+            tabla_arbitros.innerHTML += `
+            <tr>
+              <td>${doc.data().local}</td>
+              <td>${doc.data().visitante}</td>
+              <td>${doc.data().fecha}</td>
+              <td>${doc.data().hora}</td>
+              <td>${doc.data().lugar}</td>
+              <td>${doc.data().jornada}</td>
+              <td><h4 class="center"><i class="fas fa-sync-alt modal-trigger deep-purple-text text-accent-4"
+              href="#modal1" onclick="actualizarPartido('${doc.id}','${doc.data().local}',
+              '${doc.data().visitante}','${doc.data().fecha}','${doc.data().hora}','${doc.data().lugar}','${doc.data().jornada}')"></i></h4></td>
+              <td><h4 class="center"><i class="fas fa-trash-alt red-text text-accent-4" onclick="eliminarPartido('${doc.id}')"></i></h4></td>
+            </tr>
+            `;
+        });
+        var contenedor = document.getElementById('contCarga');
+        contenedor.style.visibility = 'hidden';
+        contenedor.style.opacity = '0';
+    })
+}
+
 function eliminarPartido(id) {
     db.collection("Partido").doc(id).delete().then(function () {
         console.log("Document successfully deleted!");
@@ -85,8 +122,8 @@ function eliminarPartido(id) {
         console.error("Error removing document: ", error);
     });
 }
+
 function actualizarPartido(id, local, visitante, fecha, hora, lugar, jornada) {
-    console.log('entro');
     document.getElementById('local').value = local;
     document.getElementById('visitante').value = visitante;
     document.getElementById('fecha').value = fecha;
@@ -96,6 +133,9 @@ function actualizarPartido(id, local, visitante, fecha, hora, lugar, jornada) {
     var boton = document.getElementById('boton');
     boton.innerHTML = 'Editar';
     boton.onclick = function () {
+        var contenedor = document.getElementById('contCarga');
+        contenedor.style.visibility = 'visible';
+        contenedor.style.opacity = '100';
         var washingtonRef = db.collection("Partido").doc(id);
         var local = document.getElementById('local').value;
         var visitante = document.getElementById('visitante').value;
@@ -119,14 +159,32 @@ function actualizarPartido(id, local, visitante, fecha, hora, lugar, jornada) {
             document.getElementById('lugar').value = '';
             document.getElementById('jornada').value = '';
             boton.innerHTML = 'Guardar';
-            window.location = "Partidos.html";
+            var contenedor = document.getElementById('contCarga');
+            contenedor.style.visibility = 'hidden';
+            contenedor.style.opacity = '0';
         }).catch(function (error) {
             console.error("Error updating document: ", error);
+            var contenedor = document.getElementById('contCarga');
+            contenedor.style.visibility = 'hidden';
+            contenedor.style.opacity = '0';
         })
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function limpiar() {
+
+    document.getElementById('hora').value = '';
+    document.getElementById('fecha').value = '';
+    document.getElementById('lugar').value = '';
+    document.getElementById('jornada').value = '';
+    var boton = document.getElementById('boton');
+    boton.innerHTML = 'Guardar';
+    boton.onclick = function () {
+        crearPartido();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
-  });
+});
