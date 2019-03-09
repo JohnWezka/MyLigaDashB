82aebced3759ba2b4bb27afeb45bb6341e866dbe
josemlpacheco <josemaria08121998@gmail.com>
2019-03-09 13:42:18 -0600

    cambios

--- js/admin.js
+++ js/admin.js
@@ -31,7 +31,7 @@ function registrarAdmin() {
     storageRef.put(photo).then((data) => {
         storage.ref('admin/' + photo.name).getDownloadURL().then((url) => {
             downloadURL = url;
-            db.collection("admin").add({
+            db.collection("admin").doc(uid).set({
                 nombre: nombres,
                 apellidoMaterno: appma,
                 apellidoPaterno: apppa,
@@ -39,25 +39,17 @@ function registrarAdmin() {
                 telefono: telefono,
                 foto: downloadURL,
                 userID: uid
-            }).then(function(docRef) {
-                console.log("Document written with ID: ", docRef.id);
-                var washingtonRef = db.collection("admin").doc(docRef.id);
-                return washingtonRef.update({
-                    id: docRef.id
-                }).then(function() {
-                    alert("Perfil Completo");
-                    location.href = "../pages/crear-liga.html";
-                    document.getElementById('nom').value = '';
-                    document.getElementById('appma').value = '';
-                    document.getElementById('apppa').value = '';
-                    document.getElementById('dicc').value = '';
-                    document.getElementById('tel').value = '';
-                }).catch(function(error) {
-                    // The document probably doesn't exist.
-                    console.error("Error updating document: ", error);
-                });
+            }).then(function() {
+                alert("Perfil Completo");
+                location.href = "../pages/crear-liga.html";
+                document.getElementById('nom').value = '';
+                document.getElementById('appma').value = '';
+                document.getElementById('apppa').value = '';
+                document.getElementById('dicc').value = '';
+                document.getElementById('tel').value = '';
             }).catch(function(error) {
-                console.error("Error adding document: ", error);
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
             });
         }).catch((error) => {
             console.log("url error");
@@ -68,47 +60,4 @@ function registrarAdmin() {
         console.log("error");
         console.log(error);
     });
-}
-
-function eliminarLiga(id) {
-    db.collection("ligas").doc(id).delete().then(function() {
-        console.log("Document successfully deleted!");
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
-    });
-}
-
-function actualizarLiga(id, nomLiga, nomDueno, desc, foto) {
-
-    document.getElementById('nomLiga').value = nomLiga;
-    document.getElementById('nomDueno').value = nomDueno;
-    document.getElementById('descripcion').value = desc;
-    document.getElementById('foto').value = foto;
-    var boton = document.getElementById('boton');
-    boton.innerHTML = 'Editar';
-
-    boton.onclick = function() {
-        var washingtonRef = db.collection("ligas").doc(id);
-
-        var nomLiga = document.getElementById('nomLiga').value;
-        var nomDueno = document.getElementById('nomDueno').value;
-        var desc = document.getElementById('descripcion').value;
-
-        return washingtonRef.update({
-                nombreLiga: nomLiga,
-                nombreDueno: nomDueno,
-                descripcion: desc
-            })
-            .then(function() {
-                console.log("Document successfully updated!");
-                document.getElementById('nomLiga').value = '';
-                document.getElementById('nomDueno').value = '';
-                document.getElementById('descripcion').value = '';
-                boton.innerHTML = 'Guardar';
-            })
-            .catch(function(error) {
-                // The document probably doesn't exist.
-                console.error("Error updating document: ", error);
-            });
-    }
 }
