46c55c2f1e43cf430738ce90cb89360f977b02bb
AlanOsvaldo1 <alan.mayorga@alumnouttehuacan.edu.mx>
2019-03-10 15:36:01 -0600

    Alan

--- js/RegistrarPartidos.js
+++ js/RegistrarPartidos.js
@@ -10,9 +10,9 @@ var idLiga;
 
             db.collection("admin").where("userID", "==", user.uid).get().then(function (querySnapshot) {
                 querySnapshot.forEach(function (doc) {
-                    user = doc.id;
                     idLiga = doc.data().idliga;
                     leerPartidos();
+                    leerEquipos();
                 });
             });
         } else {
@@ -24,35 +24,35 @@ var idLiga;
     });
 })();
 
-(function leerEquipos() {
-    var selection_roll = document.getElementById('local');
-    var selection_roll2 = document.getElementById('visitante');
+function leerEquipos() {
+    let selection_roll = document.getElementById('local');
+    let selection_roll2 = document.getElementById('visitante');
     selection_roll.innerHTML = '';
     selection_roll2.innerHTML = '';
-    db.collection("equipos").get().then(function (querySnapshot) {
+    db.collection("equipos").where("idLiga", "==", idLiga).get().then(function (querySnapshot) {
         selection_roll.innerHTML = '';
         selection_roll2.innerHTML = '';
         querySnapshot.forEach((doc) => {
             selection_roll.innerHTML += ` 
-            <option>${doc.data().nombreEquipo}</option>`;
+            <option value = "${doc.data().idEquipo}">${doc.data().nombreEquipo} - ${doc.data().nombreCategoria}</option>`;
             selection_roll2.innerHTML += ` 
-            <option>${doc.data().nombreEquipo}</option>`;
+            <option value = "${doc.data().idEquipo}">${doc.data().nombreEquipo} - ${doc.data().nombreCategoria}</option>`;
         });
 
     });
-})();
+}
 
 function crearPartido() {
     var contenedor = document.getElementById('contCarga');
     contenedor.style.visibility = 'visible';
     contenedor.style.opacity = '100';
 
-    var local = document.getElementById('local').value;
-    var visitante = document.getElementById('visitante').value;
-    var fecha = document.getElementById('fecha').value;
-    var hora = document.getElementById('hora').value;
-    var lugar = document.getElementById('lugar').value;
-    var jornada = document.getElementById('jornada').value;
+    let local = document.getElementById('local').value;
+    let visitante = document.getElementById('visitante').value;
+    let fecha = document.getElementById('fecha').value;
+    let hora = document.getElementById('hora').value;
+    let lugar = document.getElementById('lugar').value;
+    let jornada = document.getElementById('jornada').value;
 
     db.collection("Partido").add({
         local: local,
@@ -96,20 +96,32 @@ function leerPartidos() {
     db.collection("Partido").where("idLiga", "==", idLiga).onSnapshot(function (querySnapshot) {
         tabla_arbitros.innerHTML = '';
         querySnapshot.forEach(function (doc) {
-            tabla_arbitros.innerHTML += `
-            <tr>
-              <td>${doc.data().local}</td>
-              <td>${doc.data().visitante}</td>
-              <td>${doc.data().fecha}</td>
-              <td>${doc.data().hora}</td>
-              <td>${doc.data().lugar}</td>
-              <td>${doc.data().jornada}</td>
-              <td><h4 class="center"><i class="fas fa-sync-alt modal-trigger deep-purple-text text-accent-4"
-              href="#modal1" onclick="actualizarPartido('${doc.id}','${doc.data().local}',
-              '${doc.data().visitante}','${doc.data().fecha}','${doc.data().hora}','${doc.data().lugar}','${doc.data().jornada}')"></i></h4></td>
-              <td><h4 class="center" href=""><i class="fas fa-trash-alt red-text text-accent-4" onclick="eliminarPartido('${doc.id}')"></i></h4></td>
-            </tr>
-            `;
+            var docRef = db.collection("equipos").doc(doc.data().local);
+            docRef.get().then(function (doc1) {
+                var local = doc1.data().nombreEquipo + "\n - " + doc1.data().nombreCategoria;
+                var docRef2 = db.collection("equipos").doc(doc.data().visitante);
+            docRef2.get().then(function (doc2) {
+                var visitante = doc2.data().nombreEquipo + "\n - " + doc2.data().nombreCategoria;
+                tabla_arbitros.innerHTML += `
+                <tr>
+                <td>${local}</td>
+                <td>${visitante}</td>
+                <td>${doc.data().fecha}</td>
+                <td>${doc.data().hora}</td>
+                <td>${doc.data().lugar}</td>
+                <td>${doc.data().jornada}</td>
+                <td><h4 class="center"><i class="fas fa-sync-alt modal-trigger deep-purple-text text-accent-4"
+                href="#modal1" onclick="actualizarPartido('${doc.id}','${doc.data().local}',
+                '${doc.data().visitante}','${doc.data().fecha}','${doc.data().hora}','${doc.data().lugar}','${doc.data().jornada}')"></i></h4></td>
+                <td><h4 class="center" href=""><i class="fas fa-trash-alt red-text text-accent-4" onclick="eliminarPartido('${doc.id}')"></i></h4></td>
+                </tr>
+                `;
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
         });
         var contenedor = document.getElementById('contCarga');
         contenedor.style.visibility = 'hidden';
