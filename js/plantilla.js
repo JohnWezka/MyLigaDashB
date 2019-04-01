79cc71b8613982496d85866e592452fcf8c31bc3
unknown <josemaria08121998@gmail.com>
2019-04-01 16:12:22 -0600

    Chema

--- js/plantilla.js
+++ js/plantilla.js
@@ -58,7 +58,7 @@
                     <a class="dropdown-item" href="#">Settings</a>
                     <a class="dropdown-item" href="#">Activity Log</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" onclick="logout()">Logout</a>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
                 </div>
             </li>
         </ul>
@@ -101,7 +101,7 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <button class="btn btn-primary" type="button" data-dismiss="modal" onclick="logout()">Salir</button>
                 </div>
             </div>
         </div>
